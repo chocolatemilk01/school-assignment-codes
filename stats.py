6bb9fab8af9711c0eb35db2659b922ba693e0754
choco_milk <106296453+chocolatemilk01@users.noreply.github.com>
2022-07-14 11:18:47 +0700

    Add files via upload

---
+++ stats.py
@@ -0,0 +1,10 @@
+class stats :
+    def __init__(self,ai_game) :
+        self.settings=ai_game.settings
+        self.reset_stats()
+        self.game_active=False
+        self.highest=0
+    def reset_stats(self) :
+        self.ship_left=self.settings.ship_limit
+        self.score=0
+        self.level=1
